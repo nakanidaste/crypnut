d028c2658d4d0a7e91dcb4c8c00a85bb4cf5c672
Lullaby16 <samuelys945@gmail.com>
2022-04-07 11:56:39 +0700

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+import Footer from "./Footer";
+import Loader from "./Loader";
+import Navbar from "./Navbar";
+import Services from "./Services";
+import Transactions from "./Transactions";
+import Welcome from "./Welcome";
+
+export { Footer, Loader, Navbar, Services, Transactions, Welcome };
